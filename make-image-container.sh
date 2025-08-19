c234f524f88840b4ccd4574bb5037b84c189d27f
Mikko Salomäki <ms@akkodis.se>
2025-08-19 13:45:40 +0200

    make-image-container: use container-util for operating on container

--- make-image-container.sh
+++ make-image-container.sh
@@ -196,47 +196,9 @@ if [ "x$postinstall" != "x" ]; then
 	artifacts="${artifacts} ${build}/postinstall ${build}/input/${postinstall_basename}"
 fi
 
-# Create squashfs image
-mksquashfs $artifacts "${build}/container.squashfs" -noappend -all-root || die "Failed creating squashfs"
-
-# Create verity data
-veritysetup --data-block-size=4096 --hash-block-size=4096 format \
-	--root-hash-file "${build}/container.roothash" "${build}/container.squashfs" \
-	"${build}/container.hashtree" || die "Failed dm-verity formatting"
-
-# Sign root hash with provided method and extract public key
-if [ "x$keyfile" != "x" ]; then
-	openssl pkey -in "$keyfile" -out "${build}/container.key" -pubout -outform DER || die "Failed getting pubkey from key"
-	openssl dgst -sha256 -out "${build}/container.digest" -sign "$keyfile" "${build}/container.roothash" || die "Failed signing roothash"
-fi
-
-# Calculate offsets in file
-squashfs_size="$(stat -c %s ${build}/container.squashfs)" || die "Failed getting squashfs size"
-tree_size="$(stat -c %s ${build}/container.hashtree)" || die "Failed getting hashtree size"
-root_size="$(stat -c %s ${build}/container.roothash)" || die "Failed getting roothash size"
-digest_size="$(stat -c %s ${build}/container.digest)" || die "Failed getting digest size"
-key_size="$(stat -c %s ${build}/container.key)" || die "Failed getting public key size"
-tree_offset=$(( $squashfs_size ))
-root_offset=$(( $tree_offset + $tree_size ))
-digest_offset=$(( $root_offset + $root_size ))
-key_offset=$(( $digest_offset + $digest_size ))
-
-echo "Container blob offsets:"
-printf " 0x%08x squashfs - %d b\n" 0 $squashfs_size
-printf " 0x%08x tree     - %d b\n" $tree_offset $tree_size
-printf " 0x%08x root     - %d b\n" $root_offset $root_size
-printf " 0x%08x digest   - %d b\n" $digest_offset $digest_size
-printf " 0x%08x key      - %d b\n" $key_offset $key_size
-
-printf "%08x" 0x21474d49 | xxd -r -p > "${build}/container.offsets" || die "Failed writing offset"
-dd if=/dev/zero of="${build}/container.offsets" bs=28 count=1 oflag=append conv=notrunc
-printf "%016x" "$tree_offset" | sed 's@\(..\)\(..\)\(..\)\(..\)\(..\)\(..\)\(..\)\(..\)@0x\8\7\6\5\4\3\2\1@' | xxd -r -p >> "${build}/container.offsets" || die "Failed writing offset"
-printf "%016x" "$root_offset" | sed 's@\(..\)\(..\)\(..\)\(..\)\(..\)\(..\)\(..\)\(..\)@0x\8\7\6\5\4\3\2\1@' | xxd -r -p >> "${build}/container.offsets" || die "Failed writing offset"
-printf "%016x" "$digest_offset" | sed 's@\(..\)\(..\)\(..\)\(..\)\(..\)\(..\)\(..\)\(..\)@0x\8\7\6\5\4\3\2\1@' | xxd -r -p >> "${build}/container.offsets" || die "Failed writing offset"
-printf "%016x" "$key_offset" | sed 's@\(..\)\(..\)\(..\)\(..\)\(..\)\(..\)\(..\)\(..\)@0x\8\7\6\5\4\3\2\1@' | xxd -r -p >> "${build}/container.offsets" || die "Failed writing offset"
-
-cat "${build}/container.squashfs" "${build}/container.hashtree" "${build}/container.roothash" \
-	"${build}/container.digest" "${build}/container.key" "${build}/container.offsets" \
-	> "${build}/${container_name}" || die "Failed assembling container"
+# Create squashfs
+mksquashfs $artifacts "${build}/${container_name}" -noappend -all-root || die "Failed creating squashfs"
+# Package as container
+PATH="$path:$PATH" container-util --create --keyfile "$keyfile" "${build}/${container_name}" || die "Failed creating container"
 
 exit 0
