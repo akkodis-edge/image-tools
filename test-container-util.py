409f3f4105f3b7a8005578b0638116ed38e6cf93
Mikko Salomaki <ms@akkodis.se>
2026-04-17 11:12:18 +0200

    test-container-util: add initial cms verify

--- test-container-util.py
+++ test-container-util.py
@@ -6,9 +6,11 @@ import os
 import subprocess
 import struct
 import shutil
+import datetime
 from subprocess import CalledProcessError
+from cryptography import x509
 from cryptography.hazmat.primitives.asymmetric import rsa, ec
-from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives import serialization, hashes
 
 class ENOENT(RuntimeError):
     pass
@@ -37,14 +39,18 @@ def container_util(args):
         raise EUNKNOWN(r.stdout + r.stderr)
     return r.stdout
 
-def container_util_verify(container, public_key=None, public_dir=None):
+def container_util_verify(container, public_key=None, public_dir=None, public_key_ca=None):
     args = ['--verify',  container]
+    extra = []
     if public_key != None:
-        args.extend(['--pubkey', public_key])
-    elif public_dir != None:
-        args.extend(['--pubkey-dir', public_dir])
-    else:
-        args.append('--pubkey-any')
+        extra.extend(['--pubkey', public_key])
+    if public_dir != None:
+        extra.extend(['--pubkey-dir', public_dir])
+    if public_key_ca != None:
+        extra.extend(['--pubkey-ca', public_key_ca])
+    if not extra:
+        extra.append('--pubkey-any')
+    args.extend(extra)
     return container_util(args)
 
 def container_util_create(file, private_key):
@@ -84,13 +90,71 @@ def generate_ec_keypair(curve, priv_format=serialization.PrivateFormat.Tradition
     )
     return private_pem, public_der
 
+def generate_cert_selfigned():
+    pkey = ec.generate_private_key(ec.SECP256R1())
+    subject = x509.Name([
+                x509.NameAttribute(x509.oid.NameOID.COMMON_NAME, 'root CA')
+                ])
+    builder = x509.CertificateBuilder()
+    builder = builder.subject_name(subject)
+    builder = builder.issuer_name(subject)
+    builder = builder.public_key(pkey.public_key())
+    builder = builder.serial_number(x509.random_serial_number())
+    builder = builder.not_valid_before(
+            datetime.datetime.now(datetime.timezone.utc) - datetime.timedelta(days=1))
+    builder = builder.not_valid_after(
+            datetime.datetime.now(datetime.timezone.utc) + datetime.timedelta(days=1))
+    builder = builder.add_extension(
+        x509.BasicConstraints(ca=True, path_length=1),
+        critical=True)
+    builder = builder.add_extension(
+        x509.KeyUsage(
+            digital_signature=True,
+            content_commitment=False,
+            key_encipherment=False,
+            data_encipherment=False,
+            key_agreement=False,
+            key_cert_sign=True,
+            crl_sign=False,
+            encipher_only=False,
+            decipher_only=False,
+            ),
+        critical=True)
+    builder = builder.add_extension(
+        x509.ExtendedKeyUsage([x509.ExtendedKeyUsageOID.EMAIL_PROTECTION]),
+        critical=False)
+    builder = builder.add_extension(
+        x509.SubjectKeyIdentifier.from_public_key(pkey.public_key()),
+        critical=False)
+    builder = builder.add_extension(
+        x509.AuthorityKeyIdentifier.from_issuer_public_key(pkey.public_key()),
+        critical=False)
+    cert = builder.sign(pkey, hashes.SHA256())
+    return pkey, cert
+
 def generate_file(path, size):
     with open(path, mode='wb') as f:
         f.write(b'\x7a' * size)
 
 def write_file(path, data):
+    # Serialize if needed
+    if isinstance(data, ec.EllipticCurvePrivateKey):
+        out = data.private_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PrivateFormat.PKCS8,
+            encryption_algorithm=serialization.NoEncryption()
+        )
+    elif isinstance(data, x509.Certificate):
+        out = data.public_bytes(
+            encoding=serialization.Encoding.PEM
+        )
+    elif isinstance(data, (str, bytes, bytearray)):
+        out = data
+    else:
+        raise RuntimeError('Unsupported type for writing: {}'.format(type(data)))
+
     with open(path, mode='wb') as f:
-        f.write(data)
+        f.write(out)
 
 def dmverity_format(roothash, tree, data):
     args = ['/usr/sbin/veritysetup', '--data-block-size=4096', '--hash-block-size=4096',
@@ -106,23 +170,37 @@ def sign_data(key, data, digest, hash='sha256', extra=['-pkeyopt', 'rsa_padding_
     r = subprocess.run(args, capture_output=True, text=True, check=True)
     return r.stdout
 
+def sign_cms(key, cert, certfile, data, out, hash='sha256'):
+    args = ['openssl', 'cms', '-sign', '-in', data, '-md' ,hash, '-signer', cert,
+            '-inkey', key, '-out', out, '-outform', 'der', '-binary', '-nosmimecap',
+            '-nodetach']
+    if certfile:
+        args.extend(['-certfile', certfile])
+    r = subprocess.run(args, capture_output=True, text=True, check=True)
+    return r.stdout
+
 def make_header(path, data, tree, roothash, digest, public_key):
     with open(path, mode='wb') as f:
         f.write(struct.pack('<L', 0x494d4721))
         f.write(bytes(28))
         tree_offset = os.path.getsize(data)
         f.write(struct.pack('<Q', tree_offset))
-        roothash_offset = tree_offset + os.path.getsize(tree)
+        roothash_offset = tree_offset + os.path.getsize(tree) if roothash else 0
         f.write(struct.pack('<Q', roothash_offset))
-        digest_offset = roothash_offset + os.path.getsize(roothash)
+        digest_offset = roothash_offset + os.path.getsize(roothash) if digest else 0
         f.write(struct.pack('<Q', digest_offset))
-        public_key_offset = digest_offset + os.path.getsize(digest)
+        public_key_offset = digest_offset + os.path.getsize(digest) if digest else tree_offset + os.path.getsize(tree)
         f.write(struct.pack('<Q', public_key_offset))
 
 def assemble_file(path, data, tree, roothash, digest, public_key, header):
+    args = ['cat', data, tree]
+    if roothash:
+        args.append(roothash)
+    if digest:
+        args.append(digest)
+    args.extend([public_key, header])
     with open(path, mode='wb') as f:
-        subprocess.run(['cat', data, tree, roothash, digest, public_key, header],
-                       stdout=f, check=True)
+        subprocess.run(args, stdout=f, check=True)
 
 class test_verify(unittest.TestCase):
     @classmethod
@@ -161,6 +239,17 @@ class test_verify(unittest.TestCase):
         os.mkdir(public_dir)
         shutil.copy(self.public_key, public_dir)
         self.assertIn('File verified OK', container_util_verify(self.container, public_dir=public_dir))
+    def test_cms_ok(self):
+        self.ca_key, self.ca_cert = generate_cert_selfigned()
+        self.ca_key_path = os.path.join(self.dir, 'ca_key')
+        self.ca_cert_path = os.path.join(self.dir, 'ca_cert')
+        self.cms_path = os.path.join(self.dir, 'cms')
+        write_file(self.ca_key_path, self.ca_key)
+        write_file(self.ca_cert_path, self.ca_cert)
+        sign_cms(self.ca_key_path, self.ca_cert_path, None, self.roothash, self.cms_path)
+        make_header(self.header, self.data, self.tree, None, None, self.cms_path)
+        assemble_file(self.container, self.data, self.tree, None, None, self.cms_path, self.header)
+        self.assertIn('File verified OK', container_util_verify(self.container, public_key_ca=self.ca_cert_path))
     def test_ok_pubkey_dir_multiple_keys(self):
         make_header(self.header, self.data, self.tree, self.roothash, self.digest, self.public_key)
         assemble_file(self.container, self.data, self.tree, self.roothash, self.digest, self.public_key, self.header)
